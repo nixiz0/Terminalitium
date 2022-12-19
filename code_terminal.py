d93dee39e50f38380a9e9d281d3dfd0673195b56
Julian <julian.57@outlook.fr>
2022-12-19 17:12:07 +0100

    add run() fonction

--- code_terminal.py
+++ code_terminal.py
@@ -1,3 +1,6 @@
 # subprocess permet d'avoir accès à toutes les commandes bash de terminal
 import subprocess
 
+# Ancienne version : Popen
+# Nouvelle version : run()
+subprocess.run("dir", shell="True")
