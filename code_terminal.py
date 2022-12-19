e1c4b9353af4ffc6abdc711d491b3121c20af866
Julian <julian.57@outlook.fr>
2022-12-19 17:28:25 +0100

    add the cd fonction on the terminal

--- code_terminal.py
+++ code_terminal.py
@@ -1,16 +1,24 @@
 # subprocess permet d'avoir accès à toutes les commandes bash de terminal
 import subprocess
+import os
 
 # Ancienne version : Popen
 # Nouvelle version : run()
 
+#os.getcwd() permet d'avoir le chemin d'accès au dossier où l'on se trouve
+#os.chdir(..) permet de changer de directory (de dossier)
+
 while True:
-    commande = input("Commande : ")
+    commande = input(os.getcwd() + " --8> ")
     if commande == "exit":
         print("Arrêt du terminal")
         break
-    
-    result = subprocess.run(commande, shell="True", capture_output=True, universal_newlines=True)
-    
-    print(result.stdout)
-    print(result.stderr)
+
+    commande_split = commande.split(" ")
+    if len(commande_split) == 2 and commande_split[0] == "cd":
+        os.chdir(commande_split[1])
+    else:
+        result = subprocess.run(commande, shell="True", capture_output=True, universal_newlines=True)
+        
+        print(result.stdout)
+        print(result.stderr)
